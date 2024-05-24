1df9643e33f779c43c404fecbcde224def3a1175
Jishnu <jsbaruah1@gmail.com>
2024-05-24 14:45:48 +0530

    maybe final version

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet, Text, View, Switch, TouchableOpacity, TextInput } from 'react-native';
 import Video from 'react-native-video';
 
-
 const SocketStatusIndicator = ({ connected }) => {
   return (
     <View style={[styles.socketStatus, { backgroundColor: connected ? '#4CAF50' : '#F44336' }]}>
@@ -16,7 +15,8 @@ const App = () => {
   const [ip, setIp] = useState("192.168.47.212");
   const [data, setData] = useState({ temperature: '--', water_level: '--' });
   const [light, setLight] = useState(false);
-  const [pumps, setPumps] = useState(false);
+  const [fillTank, setFillTank] = useState(false);
+  const [emptyTank, setEmptyTank] = useState(false);
   const [dispenser, setDispenser] = useState(false);
   const [feedingTime, setFeedingTime] = useState('');
   const [waterChangeInterval, setWaterChangeInterval] = useState('');
@@ -104,14 +104,14 @@ const App = () => {
         style={styles.video}
         fullscreenOrientation={'portrait'}
       />
-      <Text style={styles.title}>Fish Tank</Text>
+      <Text style={styles.title}>Fish Tank Control</Text>
       <View style={styles.sensorData}>
         <View style={{ alignItems: 'center' }}>
           <Text style={[styles.sensorText, { color: 'white' }]}>Temperature</Text>
           <Text style={{ fontSize: 16, color: 'white' }}>{data.temperature} °C</Text>
         </View>
         <View style={{ alignItems: 'center' }}>
-          <Text style={[styles.sensorText, { color: 'white' }]}>Distance</Text>
+          <Text style={[styles.sensorText, { color: 'white' }]}>Water Level</Text>
           <Text style={{ fontSize: 16, color: 'white' }}>{data.water_level} cm</Text>
         </View>
       </View>
@@ -120,23 +120,31 @@ const App = () => {
           <Switch
             value={light}
             onValueChange={() => handleToggle(setLight, light, 'pin1')}
-            style={{ transform: [{ scale: 1.5 }] }} 
+            style={{ transform: [{ scale: 1.5 }] }}
           />
           <Text style={[styles.controlLabel, { color: 'white' }]}>Light</Text>
         </View>
         <View style={styles.control}>
           <Switch
-            value={pumps}
-            onValueChange={() => handleToggle(setPumps, pumps, 'pin2')}
-            style={{ transform: [{ scale: 1.5 }] }} 
+            value={fillTank}
+            onValueChange={() => handleToggle(setFillTank, fillTank, 'pin3')}
+            style={{ transform: [{ scale: 1.5 }] }}
           />
-          <Text style={[styles.controlLabel, { color: 'white' }]}>Pumps</Text>
+          <Text style={[styles.controlLabel, { color: 'white' }]}>Fill Tank</Text>
+        </View>
+        <View style={styles.control}>
+          <Switch
+            value={emptyTank}
+            onValueChange={() => handleToggle(setEmptyTank, emptyTank, 'pin2')}
+            style={{ transform: [{ scale: 1.5 }] }}
+          />
+          <Text style={[styles.controlLabel, { color: 'white' }]}>Empty Tank</Text>
         </View>
         <View style={styles.control}>
           <Switch
             value={dispenser}
-            onValueChange={() => handleToggle(setDispenser, dispenser, 'pin3')}
-            style={{ transform: [{ scale: 1.5 }] }} 
+            onValueChange={() => handleToggle(setDispenser, dispenser, 'pin4')}
+            style={{ transform: [{ scale: 1.5 }] }}
           />
           <Text style={[styles.controlLabel, { color: 'white' }]}>Dispenser</Text>
         </View>
@@ -145,26 +153,26 @@ const App = () => {
       <View style={styles.timeSettings}>
         <TextInput
           style={styles.input}
-          placeholder="Feeding Time(HH:MM)"
+          placeholder="Feeding Time (HH:MM)"
           value={feedingTime}
           onChangeText={setFeedingTime}
           placeholderTextColor="white"
         />
         <TouchableOpacity style={styles.button} onPress={setFeedingTimeHandler}>
-          <Text style={styles.buttonText}> + </Text>
+          <Text style={styles.buttonText}>Set</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.timeSettings}>
         <TextInput
           style={styles.input}
-          placeholder="Pump Interval(hours)"
+          placeholder="Water Change Interval (hours)"
           keyboardType="numeric"
           value={waterChangeInterval}
           onChangeText={setWaterChangeInterval}
           placeholderTextColor="white"
         />
         <TouchableOpacity style={styles.button} onPress={setWaterChangeIntervalHandler}>
-          <Text style={styles.buttonText}> + </Text>
+          <Text style={styles.buttonText}>Set</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.ipContainer}>
@@ -173,25 +181,38 @@ const App = () => {
           placeholder="Enter IP address"
           value={ip}
           onChangeText={setIp}
+          placeholderTextColor="white"
         />
-        <View style={{ marginBottom: 40 }}></View>
         <SocketStatusIndicator connected={socketConnected} />
       </View>
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
+    backgroundColor: '#282c34',
     alignItems: 'center',
+    // justifyContent: 'center',
+    padding: 20,
   },
   title: {
     fontSize: 32,
     fontWeight: 'bold',
     marginBottom: 20,
     color: 'white',
+  },
+  sensorData: {
+    marginBottom: 20,
+    flexDirection: 'row',
+  },
+  sensorText: {
+    fontSize: 24,
+    marginBottom: 5,
+    color: 'white',
+    marginHorizontal: 30,
+    fontWeight: 'bold',
   },
   controls: {
     justifyContent: 'center',
@@ -207,17 +228,6 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     fontSize: 18,
   },
-  sensorData: {
-    marginBottom: 20,
-    flexDirection: 'row',
-  },
-  sensorText: {
-    fontSize: 24,
-    marginBottom: 5,
-    color: 'white',
-    marginHorizontal: 30,
-    fontWeight: 'bold',
-  },
   timeSettings: {
     marginBottom: 30,
     width: '80%',
@@ -226,31 +236,25 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     borderWidth: 1,
     borderColor: '#ccc',
-
     borderRadius: 10,
   },
   input: {
-    // borderWidth: 1,
-    // borderColor: '#ccc',
-    // padding: 10,
     marginHorizontal: 10,
     width: '60%',
     borderRadius: 10,
     color: 'white',
-  },
-  button: {
-    // backgroundColor: '#2979FF',
-
+    padding: 10,
     // borderWidth: 1,
     borderColor: '#ccc',
-    // padding: 10,
-    width: 25,
-    borderRadius: 30,
-    marginHorizontal: 10,
-    marginLeft: 45,
+  },
+  button: {
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: '#4CAF50',
+    marginLeft: 10,
   },
   buttonText: {
-    fontSize: 30,
+    fontSize: 18,
     color: 'white',
   },
   video: {
@@ -261,34 +265,30 @@ const styles = StyleSheet.create({
     right: 0,
   },
   ipInput: {
-    // padding: 10,
-    // width: '60%',
-
+    padding: 10,
+    width: '60%',
     borderRadius: 10,
     textAlign: 'center',
     color: 'white',
+    // borderWidth: 1,
+    borderColor: '#ccc',
   },
   ipContainer: {
     marginBottom: 20,
     width: '80%',
-    alignItems: 'center',
     flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
     borderColor: '#ccc',
-    // padding: 10,
-
     borderRadius: 10,
   },
   socketStatus: {
     width: 20,
     height: 20,
     borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginLeft: 30,
+    marginLeft: 10,
   },
 });
-
 
 export default App;
